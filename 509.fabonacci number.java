13e234f718f5ad1c8c48a61ae26620ec322eb8b2
25ajayrawat25-sketch <25ajayrawat25@dhaanishcollege.in>
2026-07-23 14:25:17 +0530

    Add Fibonacci number calculation in Java

---
+++ 509.fabonacci number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int fib(int n) {
+        // Base cases
+        if (n <= 1) {
+            return n;
+        }
+
+        int prev2 = 0; // F(0)
+        int prev1 = 1; // F(1)
+        int current = 0;
+
+        // Iteratively compute F(n) using space optimization
+        for (int i = 2; i <= n; i++) {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return current;
+    }
+}
